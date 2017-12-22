4b9731b36f427d94bea8dba9103c21145e2f8946
Andrew Schorr <afs35mm@gmail.com>
2017-12-22 17:38:14 -0500

    making moar better :)

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # AndrewFSchorr.com
 
-New personal site with all the es6/webpack/HMR/hawtness
-
 Feel free to use. :)
 
 ## License
--- src/js/get-git-messages.js
+++ src/js/get-git-messages.js
@@ -4,25 +4,25 @@ import moment from 'moment';
 const endpoint = 'https://api.github.com/users/andrewfschorr/events';
 
 function getRecentGitCommits(cb) {
-  fetch(endpoint).then(resp => resp.json()).then((data) => {
-    const pushEvents = data.filter((evt) => {
-      return evt.type === 'PushEvent';
-    });
-    const commitObjs = [];
-    pushEvents.forEach((pushEvt) => {
-      pushEvt.payload.commits.forEach((commit) => {
-        commitObjs.push({
-          commit: commit.message,
-          dateTime: moment(pushEvt.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a'),
-          repo: pushEvt.repo.name,
-        })
-      })
-    });
+    fetch(endpoint).then(resp => resp.json()).then((data) => {
+        const pushEvents = data.filter((evt) => {
+            return evt.type === 'PushEvent';
+        });
+        const commitObjs = [];
+        pushEvents.forEach((pushEvt) => {
+            pushEvt.payload.commits.forEach((commit) => {
+                commitObjs.push({
+                    commit: commit.message,
+                    dateTime: moment(pushEvt.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a'),
+                    repo: pushEvt.repo.name,
+                })
+            })
+        });
     cb(commitObjs);
-  }).catch((err) => {
-    cb(data);
-    throw new Error(err);
-  });
+    }).catch((err) => {
+        cb(data);
+        throw new Error(err);
+    });
 }
 
 export default getRecentGitCommits;
