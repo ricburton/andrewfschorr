48a901593a86fb7141552e6f2cace986bc407428
Andrew Schorr <afs35mm@gmail.com>
2017-12-23 15:31:45 -0500

    added desktop bg transition

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 
 Feel free to use. :)
 
-`git push grid master`
-
 ## License
 MIT
--- src/js/background-desktop.js
+++ src/js/background-desktop.js
@@ -1,5 +1,11 @@
 export default class backgroundDesktop {
-  constructor() {
+    constructor(changeBgFn) {
+        this.changeBgFn = changeBgFn;
+        document.addEventListener('mousemove', this.transitionMove.bind(this));
+    }
 
-  }
+    transitionMove(e) {
+        const percentageWidth = e.screenX / window.innerWidth;
+        this.changeBgFn(percentageWidth);
+    }
 }
